c93408111d11c47efcb8a9e55cab0c50f95a59ee
jorge <jvr1982@gmail.com>
2022-11-30 12:50:13 +0100

    prueba

---
+++ PruebaAuren/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using PruebaAuren.Entities;
+using System;
+using System.Collections.Generic;
+
+
+namespace PruebaAuren
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+        {
+            var userRepository = new UserRepository();
+            var usuarios = userRepository.GetUsers();
+            foreach (var usuario in usuarios)
+            {
+                Console.WriteLine(usuario.email);
+            }
+        }
+
+    }
+}
---
+++ PruebaAuren/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using PruebaAuren.Entities;
+using System;
+using System.Collections.Generic;
+
+
+namespace PruebaAuren
+{
+    public class UserRepository
+    {
+        public  List<User> GetUsers()
+        {
+            
+                using (var webClient = new System.Net.WebClient())
+                {
+                    var json = webClient.DownloadString("https://jsonplaceholder.typicode.com/users");
+                    List<User> usuarios = JsonConvert.DeserializeObject<List<User>>(json);
+
+                    return usuarios;
+                }         
+
+        }
+    }
+}
